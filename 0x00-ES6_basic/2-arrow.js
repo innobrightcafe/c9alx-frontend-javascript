2583d2ee2ae0d7578e7327921480ff6ab12837bd
inosoft <innobrightcafe@gmail.com>
2023-06-28 10:56:38 -0400

    task block testing

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+    const addNeighborhood = (newNeighborhood) => {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
+    };
+  
+    return {
+      addNeighborhood
+    };
+  }
