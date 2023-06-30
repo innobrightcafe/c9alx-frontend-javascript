0e278f7ee7b174f47dc86a30e73a680896d25ba6
inosoft <innobrightcafe@gmail.com>
2023-06-30 10:54:15 -0400

    Working with const and var

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,12 +1,12 @@
 const getNeighborhoodsList = () => {
   const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const addNeighborhood = (newNeighborhood) => {
-    sanFranciscoNeighborhoods.push(newNeighborhood);
-    return sanFranciscoNeighborhoods;
+  return {
+    addNeighborhood: (newNeighborhood) => {
+      sanFranciscoNeighborhoods.push(newNeighborhood);
+      return sanFranciscoNeighborhoods;
+    }
   };
-
-  return { addNeighborhood };
 };
 
 export default getNeighborhoodsList;
