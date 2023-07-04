dd1d9c50a066bd2c9ddf53ab2eef7565bef83b9a
inosoft <innobrightcafe@gmail.com>
2023-07-04 06:35:28 -0400

    get students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -5,16 +5,19 @@ export default function getListStudents() {
         firstName:'Guillaume',
         location: 'San Francisco'
     }
+
     const obj2 = {
         id: 2,
         firstName:'James',
         location: 'Columbia'
     }
+
     const obj3 = {
         id: 5,
         firstName:'Serena',
         location: 'San Francisco'
     }
+
     return [obj1, obj2, obj3];
   }
   
